9e7d4bc7d8636ac13c0b0e4c9c49de0cfeb28d2a
Alberto Soriano <alberto.soriano@ucuenca.edu.ec>
2023-03-31 07:46:55 -0500

    Actualizado para Power Shell

--- GenerarSSH.sh
+++ GenerarSSH.sh
@@ -1,1 +1,13 @@
+#Crea clave ssh
 ssh-keygen -t ed25519 -C "your_email@example.com"
+
+#Comprueba si el agente ssh esta activo
+Get-Service ssh-agent #Ejecuntando
+Get-Process ssh-agent #Activo
+
+#Inicia el agente si no esta activo
+Start-Service ssh-agent #Ejecuta
+ssh-agent #Activa
+
+#Agregar llave ssh
+ssh-add ~/.ssh/id_ed25519
